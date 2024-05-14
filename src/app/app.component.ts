56bde379fac3da703108b21bbc525cb9372dc9c2
Yusha Tahlil <ymmttmy5@gmail.com>
2024-05-14 17:14:18 +0530

    Pipes & Custum Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Structure Directives';
-  isLoggedIn: boolean = true;
-  username: string = 'Yusha';
-  names: string[] = [
-    'Yusha',
-    'Babu Rao',
-    'Chota babu',
-    'Jahil Babu',
-    'Gawar Babu',
-  ];
-
-  grade: string = 'B';
+  title: string = 'Pipes';
+  today: number = Date.now();
+  currency: number = 123;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { AppendPipe } from './pipes/append.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AppendPipe],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
