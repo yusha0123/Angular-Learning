2a8cdca5555da33b6bfd56570245fe4ccd324e35
Yusha Tahlil <ymmttmy5@gmail.com>
2024-05-14 18:54:24 +0530

    Dependency injection & services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { MessagesService } from './services/messages.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  providers: [MessagesService],
 })
 export class AppComponent {
-  title: string = 'Pipes';
-  today: number = Date.now();
-  currency: number = 123;
+  messages: string[] = [];
+
+  constructor(private messageService: MessagesService) {
+    this.messages = messageService.getMessages();
+  }
+
+  title: string = 'Dependancy Injection & Services';
 }
